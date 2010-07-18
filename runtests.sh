85ef118108dd1afcee028752448cb418b6161c37
ivank <ivank@ludios.net>
2010-07-18 21:40:08 +0000

    runtests.sh: also suggest using Twisted trial

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -e
 
+# If this doesn't work, or if you want to obtain coverage
+# information, install Twisted and run `trial --coverage protojson`
+
 python -W all protojson/run_tests.py
