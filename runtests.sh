fe4874428c4411e3f47cef9fb10d2e716ef10e39
ivank <ivank@ludios.net>
2010-12-03 06:52:07 +0000

    runtests.sh: test with more CPython flags, test with pypy, and test with MYPY_CONSTANT_BINDER_AUTOENABLE both on and off.

--- runtests.sh
+++ runtests.sh
@@ -3,4 +3,12 @@
 # If this doesn't work, or if you want to obtain coverage
 # information, install Twisted and run `trial --coverage protojson`
 
-python -W all protojson/run_tests.py
+MYPY_CONSTANT_BINDER_AUTOENABLE=0 time python     -W all protojson/run_tests.py
+MYPY_CONSTANT_BINDER_AUTOENABLE=1 time python     -W all protojson/run_tests.py
+MYPY_CONSTANT_BINDER_AUTOENABLE=0 time python -O  -W all protojson/run_tests.py
+MYPY_CONSTANT_BINDER_AUTOENABLE=1 time python -O  -W all protojson/run_tests.py
+MYPY_CONSTANT_BINDER_AUTOENABLE=0 time python -OO -W all protojson/run_tests.py
+MYPY_CONSTANT_BINDER_AUTOENABLE=1 time python -OO -W all protojson/run_tests.py
+
+MYPY_CONSTANT_BINDER_AUTOENABLE=0 time pypy       -W all protojson/run_tests.py
+MYPY_CONSTANT_BINDER_AUTOENABLE=1 time pypy       -W all protojson/run_tests.py
