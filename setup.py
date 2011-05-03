5b55b430a2b3f5b4e1d9054dee83e9e2f3ce8669
ivank <ivan@ludios.org>
2011-05-03 04:13:08 +0000

    setup.py: change program name: protojson -> Protojson

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 import protojson
 
 setup(
-	name='protojson',
+	name='Protojson',
 	version=protojson.__version__,
 	description="Convert any google.protobuf.message.Message to and from " +
 		"a format supported by Closure Library's goog.proto2",
