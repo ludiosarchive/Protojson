02b83183ac017e2acccb43f99483394060212caa
ivank <ivan@ludios.org>
2011-05-03 04:07:44 +0000

    Add MANIFEST.in; update setup.py for uploading to PyPI; update .gitignore

--- setup.py
+++ setup.py
@@ -9,5 +9,15 @@ setup(
 	version=protojson.__version__,
 	description="Convert any google.protobuf.message.Message to and from " +
 		"a format supported by Closure Library's goog.proto2",
+	url="https://github.com/ludios/Protojson",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 5 - Production/Stable',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
 	packages=['protojson'],
 )
