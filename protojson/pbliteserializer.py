70fb9bb18f7fda587ab481e647521dcab17698ee
Ivan Kozik <ivan@ludios.org>
2011-05-09 02:52:35 +0000

    Update to use refbinder.api instead of mypy.refbinder

--- protojson/pbliteserializer.py
+++ protojson/pbliteserializer.py
@@ -208,10 +208,6 @@ class PbLiteSerializer(object):
 
 
 
-try:
-	from mypy import refbinder
-except ImportError:
-	pass
-else:
-	refbinder.bindRecursive(sys.modules[__name__], _postImportVars)
-	del refbinder
+try: from refbinder.api import bindRecursive
+except ImportError: pass
+else: bindRecursive(sys.modules[__name__], _postImportVars)
